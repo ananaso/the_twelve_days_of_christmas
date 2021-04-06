4c59e96e020f64251f93799201fbc7455a7bc0b7
Alden Davidson <adavidson+github@protonmail.ch>
2021-04-06 10:59:37 -0700

    handled all the gift edgecases

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,44 @@
+fn capitalize_first_letter(s1: &str) -> String {
+    let mut v: Vec<char> = s1.chars().collect();
+    v[0] = v[0].to_uppercase().nth(0).unwrap();
+    let s2: String = v.into_iter().collect();
+    s2
+}
+
 fn main() {
     let days = ["first", "second", "third", "fourth", "fifth", "sixth", 
         "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"];
     let gifts = ["a partridge in a pear tree",
-                 "two turtle doves",
-                 "three French hens",
-                 "four calling birds",
-                 "five golden rings",
-                 "six geese a-laying",
-                 "seven swans a-swimming",
-                 "eight maids a-milking",
-                 "nine ladies dancing",
-                 "ten lords a-leaping",
-                 "eleven pipers piping",
-                 "twelve drummers drumming"];
+                 "Two turtle doves",
+                 "Three French hens",
+                 "Four calling birds",
+                 "Five golden rings",
+                 "Six geese a-laying",
+                 "Seven swans a-swimming",
+                 "Eight maids a-milking",
+                 "Nine ladies dancing",
+                 "Ten lords a-leaping",
+                 "Eleven pipers piping",
+                 "Twelve drummers drumming"];
 
     for day in days.iter() {
         println!("On the {} day of Christmas my true love gave to me", day);
         let count = days.iter().position(|x| x == day);
         match count {
             Some(x) => {
-                for i in (0..=x).rev() {
-                    println!("{}", gifts[i]);
+                if x == 0 {
+                    println!("{}", capitalize_first_letter(gifts[x]));
+                } else {
+                    for i in (0..=x).rev() {
+                        let gift = gifts[i];
+                        if i == 0 {
+                            println!("And {}", gift);
+                        } else {
+                            println!("{},", gifts[i]);
+                        }
+                    }
                 }
+                println!("");
             },
             None    => println!("Invalid day of Christmas!"),
         }
